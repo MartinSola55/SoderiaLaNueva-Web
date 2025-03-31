80052c0ececa933453021ac221b6ed4e91d308d1
MartinSola55 <martinrsola55@gmail.com>
2025-03-31 00:10:18 -0300

    Product sort

--- src/screens/products/Products.data.js
+++ src/screens/products/Products.data.js
@@ -1,53 +1,55 @@
 import { AddressFormatter } from "@components";
 
 export const productCols = [
-    {
-        name: 'name',
-        text: 'Nombre',
-        textCenter: true,
-    },
-    {
-        name: 'price',
-        text: 'Precio',
-        textCenter: true,
-    },
-    {
-        name: 'type',
-        text: 'Tipo',
-        textCenter: true,
-    },
-    {
-        name: 'createdAt',
-        text: 'Fecha de creación',
-        textCenter: true,
-    }
+	{
+		name: 'name',
+		text: 'Nombre',
+		textCenter: true,
+	},
+	{
+		name: 'price',
+		text: 'Precio',
+		textCenter: true,
+	},
+	{
+		name: 'type',
+		text: 'Tipo',
+		textCenter: true,
+	},
+	{
+		name: 'createdAt',
+		text: 'Fecha de creación',
+		textCenter: true,
+	}
 ];
 
 export const clientCols = [
-    {
-        name: 'name',
-        text: 'Cliente',
-        textCenter: true,
-    },
-    {
-        name: 'address',
-        text: 'Dirección',
-        textCenter: true,
-        formatter: AddressFormatter,
-    },
-    {
-        name: 'route',
-        text: 'Reparto',
-        textCenter: true,
-    }
+	{
+		name: 'name',
+		text: 'Cliente',
+		textCenter: true,
+	},
+	{
+		name: 'address',
+		text: 'Dirección',
+		textCenter: true,
+		formatter: AddressFormatter,
+	},
+	{
+		name: 'route',
+		text: 'Reparto',
+		textCenter: true,
+	}
 ];
 
 export const sortProductItems = [
-    { value: 'createdAt-asc', label: 'Creado - Asc.' },
-    { value: 'createdAt-desc', label: 'Creado - Desc.' },
+	{ value: 'name-asc', label: 'Nombre - Asc.' },
+	{ value: 'name-desc', label: 'Nombre - Desc.' },
+	{ value: 'createdAt-asc', label: 'Creado - Asc.' },
+	{ value: 'createdAt-desc', label: 'Creado - Desc.' },
 ];
 
 export const statusItems = [
-    { value: 'active', label: 'Activo' },
-    { value: 'inactive', label: 'Inactivo' },
+	{ value: 'active', label: 'Activo' },
+	{ value: 'inactive', label: 'Inactivo' },
 ];
